e10c5e63caea3ac7c9c830368074534fd4228110
TerryKrom <terrykrom1@gmail.com>
2024-02-04 14:14:48 -0300

    unused assets removed

--- hiphop-website/src/components/carousel.jsx
+++ hiphop-website/src/components/carousel.jsx
@@ -2,9 +2,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel as ReactCarousel } from "react-responsive-carousel";
 import "./carousel.css";
 
-import image1 from "../assets/banner01.png";
-import image2 from "../assets/banner02.png";
-import img1 from '../assets/img01.jpg'
+import image1 from "../assets/banners/banner01.png";
+import img1 from '../assets/banners/img01.jpg'
 // import image3 from "../assets/banner03.png";
 
 const slides = [
