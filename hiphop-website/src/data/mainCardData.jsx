1c3fa63a10e343f9ff06c2f3f5e4cc8251dfafbf
TerryKrom <terrykrom1@gmail.com>
2024-02-17 17:35:58 -0300

    news updated

--- hiphop-website/src/data/mainCardData.jsx
+++ hiphop-website/src/data/mainCardData.jsx
@@ -3,6 +3,7 @@ import img01 from '../assets/cardImages/img01.jpg';
 import img02 from '../assets/cardImages/img02.jpg';
 import img03 from '../assets/cardImages/img03.jpg';
 import img04 from '../assets/cardImages/img04.png';
+import img05 from '../assets/cardImages/img05.png';
 
 const MainCardsData = () => {
   const cardsData = [
@@ -15,29 +16,62 @@ const MainCardsData = () => {
       target: ''
     },
     {
-        date: '08-02-2024',
-        img: img02,
-        title: 'DKJ RAP coloca ao ar música das antigas, relembre!',
-        text: 'O Veneno da Serpente, de 2003, relançada no youtube para relembrar o estilo "das antigas"',
-        src: 'https://www.youtube.com/watch?v=6GJUPvXF9A0',
-        target: '_blank'
-      },
-      {
-        date: '07-02-2024',
-        img: img03,
-        title: 'Confira a trajetória de DKJ RAP no Youtube!',
-        text: 'Todas as músicas e albums da carreira de um dos maiores rappers da baixada santista.',
-        src: 'https://youtube.com/@DkjRapNacional',
-        target: '_blank'
-      },
-      {
-        date: '06-02-2024',
-        img: img04,
-        title: 'Novo lançamento na área! "Só os Di Verdade"',
-        text: 'Confira o mais novo lançamento de DKJ RAP no youtube, Deixe seu Like e compartilhe para ajudar!',
-        src: 'https://youtube.com/watch?v=y82Jgyrzjy4',
-        target: '_blank'
-      },
+      date: '08-02-2024',
+      img: img02,
+      title: 'DKJ RAP coloca ao ar música das antigas, relembre!',
+      text: 'O Veneno da Serpente, de 2003, relançada no youtube para relembrar o estilo "das antigas"',
+      src: 'https://www.youtube.com/watch?v=6GJUPvXF9A0',
+      target: '_blank'
+    },
+    {
+      date: '07-02-2024',
+      img: img03,
+      title: 'Confira a trajetória de DKJ RAP no Youtube!',
+      text: 'Todas as músicas e albums da carreira de um dos maiores rappers da baixada santista.',
+      src: 'https://youtube.com/@DkjRapNacional',
+      target: '_blank'
+    },
+    {
+      date: '06-02-2024',
+      img: img04,
+      title: 'Novo lançamento na área! "Só os Di Verdade"',
+      text: 'Confira o mais novo lançamento de DKJ RAP no youtube, Deixe seu Like e compartilhe para ajudar!',
+      src: 'https://youtube.com/watch?v=y82Jgyrzjy4',
+      target: '_blank'
+    },
+    {
+      date: '10-02-2024',
+      img: img05,
+      title: 'DKJ RAP agora no Spotify!',
+      text: 'Siga o novo perfil de DKJ RAP no spotify para acompanhar todos os novos lançamentos, além de poder ouvir as melhores músicas e albuns disponíveis!',
+      src: 'https://youtube.com/watch?v=y82Jgyrzjy4',
+      target: '_blank'
+    },
+    {
+      date: '02-02-2024',
+      img: img05,
+      title: 'DKJ RAP agora no Spotify!',
+      text: 'Siga o novo perfil de DKJ RAP no spotify para acompanhar todos os novos lançamentos, além de poder ouvir as melhores músicas e albuns disponíveis!',
+      src: 'https://youtube.com/watch?v=y82Jgyrzjy4',
+      target: '_blank'
+    },
+    {
+      date: '02-02-2024',
+      img: img05,
+      title: 'DKJ RAP agora no Spotify!',
+      text: 'Siga o novo perfil de DKJ RAP no spotify para acompanhar todos os novos lançamentos, além de poder ouvir as melhores músicas e albuns disponíveis!',
+      src: 'https://youtube.com/watch?v=y82Jgyrzjy4',
+      target: '_blank'
+    },
+    {
+      date: '02-02-2024',
+      img: img05,
+      title: 'DKJ RAP agora no Spotify!',
+      text: 'Siga o novo perfil de DKJ RAP no spotify para acompanhar todos os novos lançamentos, além de poder ouvir as melhores músicas e albuns disponíveis!',
+      src: 'https://youtube.com/watch?v=y82Jgyrzjy4',
+      target: '_blank'
+    },
+
     // Adicione mais objetos conforme necessário
   ];
 
